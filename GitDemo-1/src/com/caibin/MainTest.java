a0016e749efaf301ca880383c8402be788440b52
${EyeOfGold} <${173712363@qq.com}>
2021-04-01 11:00:34 +0800

    amend information again

--- GitDemo-1/src/com/caibin/MainTest.java
+++ GitDemo-1/src/com/caibin/MainTest.java
@@ -3,6 +3,6 @@ package com.caibin;
 public class MainTest {
     public static void main(String[] args){
         System.out.println("I am a main function");
-        System.out.println("I am a father of a main function");
+        System.out.println("I am a brother of a main function");
     }
 }
